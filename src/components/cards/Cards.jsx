1488f078079165e063187597bc71dce63301bcda
Juan Herrera <juannicolash01@gmail.com>
2021-08-16 15:25:59 -0300

    renderizado dinamico

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -3,14 +3,10 @@ import Card from './card/Card'
 
 function Cards() {
 
-    const [images, setImages] = useState({
-        urls: {
-            regular: "",
-        }
-    });
+    const [images, setImages] = useState([]);
 
     const peticion = async() => {
-        const res = await fetch("https://api.unsplash.com/photos/random/?client_id=Be6yJjbvPkMRPZ9v2B6EzeHmsiiOuZap4tW_kSVf2EI");
+        const res = await fetch("https://api.unsplash.com/photos/?client_id=Be6yJjbvPkMRPZ9v2B6EzeHmsiiOuZap4tW_kSVf2EI");
         const data = await res.json();
 
         setImages(data)
@@ -22,7 +18,11 @@ function Cards() {
 
     return (
         <>
-            <Card img={images.urls.regular} />
+        {
+            images.map((img) => {
+            return <Card key={img.id} img={img.urls.regular} />
+            })
+        }
         </>
     );
 }
