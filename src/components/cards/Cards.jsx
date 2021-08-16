639029bdbcd4e3e112b1b7d1c01c0ed75f4e5689
Juan Herrera <juannicolash01@gmail.com>
2021-08-16 15:05:41 -0300

    proptype card, default value of url

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -3,7 +3,11 @@ import Card from './card/Card'
 
 function Cards() {
 
-    const [images, setImages] = useState({});
+    const [images, setImages] = useState({
+        urls: {
+            regular: "",
+        }
+    });
 
     const peticion = async() => {
         const res = await fetch("https://api.unsplash.com/photos/random/?client_id=Be6yJjbvPkMRPZ9v2B6EzeHmsiiOuZap4tW_kSVf2EI");
@@ -12,15 +16,13 @@ function Cards() {
         setImages(data)
     };
  
-    useEffect(() => {
-        
+    useEffect(() => {       
         peticion();
-
     }, []);
 
     return (
         <>
-            <Card  />
+            <Card img={images.urls.regular} />
         </>
     );
 }
--- src/components/cards/card/Card.jsx
+++ src/components/cards/card/Card.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Card() {
+const Card = ({img}) => {
     return (
-        <div className="card" style={{width: '18rem'}}>
-            <img src="https://images.unsplash.com/photo-1626277787644-47d530591292?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTQxMDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjkxMzM2MzU&ixlib=rb-1.2.1&q=80&w=1080" className="card-img-top" alt="imagen.png" />
-        </div>
+        <>
+            <div className="card" style={{width: '18rem'}}>
+                <img src={img} className="card-img-top" alt="imagen.png" />
+            </div>
+        </>
     )
 }
 
+Card.propTypes = {
+    img: PropTypes.string
+}
+
 export default Card
+
+
