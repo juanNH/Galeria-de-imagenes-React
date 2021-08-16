9b2a15eb3051c0ef66810389c26b2d29dd61ec5d
Juan Herrera <juannicolash01@gmail.com>
2021-08-16 16:21:02 -0300

    forms y busqueda

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,23 +1,51 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
 import Card from './card/Card'
 
 function Cards() {
 
     const [images, setImages] = useState([]);
 
-    const peticion = async() => {
-        const res = await fetch("https://api.unsplash.com/photos/?client_id=Be6yJjbvPkMRPZ9v2B6EzeHmsiiOuZap4tW_kSVf2EI");
+    const [input, setInput] = useState("");
+
+    const peticion = useCallback(async() => {
+        
+        const key = "client_id=Be6yJjbvPkMRPZ9v2B6EzeHmsiiOuZap4tW_kSVf2EI"
+     
+        let route = `https://api.unsplash.com/photos/?${key}`
+        
+        if(input !== "") {
+            route = `https://api.unsplash.com/search/photos/?query=${encodeURI(input)}&${key}`
+        }
+        const res = await fetch(route);
+        
         const data = await res.json();
 
-        setImages(data)
-    };
- 
+        if(data.results){
+            setImages(data.results)
+        }else {
+            setImages(data)
+        }
+    },[input]);
+
     useEffect(() => {       
         peticion();
-    }, []);
+    }, [peticion]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const text = e.target[0].value;
+
+        setInput(text);
+    }
 
     return (
         <>
+
+        <form onSubmit={handleSubmit}>
+            <label> {" "}Buscar <input name="inputText" type="text" placeholder="Ingrese un texto"/> {" "}</label>
+        </form>
+        <hr />
+
         {
             images.map((img) => {
             return <Card key={img.id} img={img.urls.regular} />
