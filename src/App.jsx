3c7091cd351c26d9307cc8b3fe94adc45a358d59
Juan Herrera <juannicolash01@gmail.com>
2021-08-16 14:09:07 -0300

    container y imagen ej json

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Container from './components/Container'
 const App = () => {
   return (
     <div>
       <h1>Galeria App</h1>
+      <hr />
+      <Container  />
     </div>
   )
 }
---
+++ src/components/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Container = () => {
+    return (
+        <div className="conatiner border">
+            <div className="card" style={{width: '18rem'}}>
+                <img src="https://images.unsplash.com/photo-1626277787644-47d530591292?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTQxMDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjkxMzM2MzU&ixlib=rb-1.2.1&q=80&w=1080" className="card-img-top" alt="imagen.png" />
+            </div>
+        </div>
+    )
+}
+
+export default Container
